5d059257a0cba8832243cb22ed8a982ccd55a09c
AgrbaNika <126230032+AgrbaNika@users.noreply.github.com>
2023-12-30 21:08:41 +0400

    Add files via upload

---
+++ surgboard/scrpts/tab.js
@@ -0,0 +1,8 @@
+$(".interective-avatar__link").click(e => {
+    e.preventDefault();
+
+    const $this = $(e.currentTarget);
+    const curItem = $this.closest(".reviews__switcher-item");
+
+    curItem.addClass("active").siblings().removeClass("active")
+});
